084e236741a9f60431c11795171c48f88e5c60fa
Tim-VandenBorre <69650471+Tim-VandenBorre@users.noreply.github.com>
2020-10-15 16:04:11 +0200

    Website
    Laravel 8 project, authenticatie toegevoegd(login, registratie), alle ascii karakters worden aanvaard in wachtwoord, wachtwoord minimum 7 karakters, mogelijkheid voor password manager, email verificatie, welcome message(nog niet volledig af), mogelijkheid om gegevens op te vragen, gdpr(nog niet volledig af)

---
+++ Website/resources/views/livewire/greeting.blade.php
@@ -0,0 +1,6 @@
+<div class="ml-12">
+    <div class="mt-12 text-gray-600 dark:text-gray-400 text-lg" style="text-align: center">
+        Welcome {{ Auth::user()->name }}
+    </div>
+</div>
+
---
+++ Website/routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\GDPRController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware(['auth'])->name('verification.notice');
+
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return view('dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('status', 'verification-link-sent');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+Route::get('/gdpr', [GDPRController::class, 'index'])->name('gdpr');
