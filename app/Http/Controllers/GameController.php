f0df43e8a171eec51d61a913c6113eb16f4387bb
Tim-Van-den-Borre <72195069+Tim-Van-den-Borre@users.noreply.github.com>
2020-12-17 18:19:59 +0100

    Code cleanup

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,100 +6,61 @@ use App\Models\Game;
 
 class GameController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $games = auth()->user()->games;
- 
-        return response()->json([
-            'success' => true,
-            'data' => $games
-        ]);
+        return response()->json(['success' => true, 'data' => $games]);
     }
  
-    public function show($id)
-    {
+    public function show($id) {
         $game = auth()->user()->games()->find($id);
  
         if (!$game) {
-            return response()->json([
-                'success' => false,
-                'message' => 'game not found '
-            ], 400);
+            return response()->json(['success' => false, 'message' => 'The specified game has not been found.'], 400);
         }
- 
-        return response()->json([
-            'success' => true,
-            'data' => $game->toArray()
-        ], 400);
+        return response()->json(['success' => true, 'data' => $game->toArray()], 400);
     }
  
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'title' => 'required',
-            'description' => 'required'
-        ]);
+    public function store(Request $request) {
+        $this->validate($request, ['title' => 'required', 'description' => 'required']);
  
         $game = new game();
         $game->title = $request->title;
         $game->description = $request->description;
  
-        if (auth()->user()->games()->save($game))
-            return response()->json([
-                'success' => true,
-                'data' => $game->toArray()
-            ]);
-        else
-            return response()->json([
-                'success' => false,
-                'message' => 'game not added'
-            ], 500);
+        if (auth()->user()->games()->save($game)) {
+            return response()->json(['success' => true, 'data' => $game->toArray()]);
+        } else {
+            return response()->json(['success' => false, 'message' => 'The game has not been added.'], 500);
+        }
     }
  
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         $game = auth()->user()->games()->find($id);
  
         if (!$game) {
-            return response()->json([
-                'success' => false,
-                'message' => 'game not found'
-            ], 400);
+            return response()->json(['success' => false, 'message' => 'The specified game has not been found.'], 400);
         }
  
         $updated = $game->fill($request->all())->save();
  
-        if ($updated)
-            return response()->json([
-                'success' => true
-            ]);
-        else
-            return response()->json([
-                'success' => false,
-                'message' => 'game can not be updated'
-            ], 500);
+        if ($updated) {
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false, 'message' => 'The specified game can not be updated.'], 500);
+        }
     }
  
-    public function destroy($id)
-    {
+    public function destroy($id) {
         $game = auth()->user()->games()->find($id);
  
         if (!$game) {
-            return response()->json([
-                'success' => false,
-                'message' => 'game not found'
-            ], 400);
+            return response()->json(['success' => false, 'message' => 'The specified game has not been found.'], 400);
         }
  
         if ($game->delete()) {
-            return response()->json([
-                'success' => true
-            ]);
+            return response()->json(['success' => true]);
         } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'game can not be deleted'
-            ], 500);
+            return response()->json(['success' => false, 'message' => 'The specified game has not been deleted.'], 500);
         }
     }
 }
